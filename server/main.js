cb8f6884ce7ce4fb769686167bbf9d4fd7cf3f91
Sanjeev Premi <spremi@ymail.com>
2017-12-16 23:38:56 +0530

    server: Add simple 'hello world' application
    ...just to test the server coming up.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ server/main.js
@@ -0,0 +1,16 @@
+/**
+ * Main server application file
+ */
+
+'use strict';
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000!');
+});
