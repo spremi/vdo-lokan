6840d54e862b110937d98e02e049527e5167908c
Angular CLI <angular-cli@angular.io>
2017-12-17 23:28:13 +0530

    chore: initial commit from @angular/cli v1.6.1
    Command:
      ng new vdo-lokan --prefix=vdo --routing --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vdo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'vdo';
+}
