b8a7fca7ab0889bbadf0dabd6a6a077100e72cfa
Sanjeev Premi <spremi@ymail.com>
2017-12-24 03:41:12 +0530

    header: Create skeleton
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  /**
+   * Is user verified?
+   */
+  userVerified = false;
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
