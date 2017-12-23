0840955a98077d84273932ea457dc68968f590e5
Sanjeev Premi <spremi@ymail.com>
2017-12-24 03:12:15 +0530

    app: Register icons via IconService
    Also imported - HttpClientModule - required for
    loading icons.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
+import { IconService } from './modules/material/icon.service';
+import { MaterialModule } from './modules/material/material.module';
 
 
 @NgModule({
@@ -12,9 +14,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class
+ AppModule {
+  constructor (private iconSvc: IconService) {
+    iconSvc.register();
+  }
+}
