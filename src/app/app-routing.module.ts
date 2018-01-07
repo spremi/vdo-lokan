bdf6b976b6bd658619a35ea2618a23432421eeb2
Sanjeev Premi <spremi@ymail.com>
2018-01-07 16:05:15 +0530

    routes: Setup basic routing
    The 'body' is set as default outlet.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './routes/about/about.component';
+import { E404Component } from './routes/e404/e404.component';
+import { HomeComponent } from './routes/home/home.component';
+import { SettingsComponent } from './routes/settings/settings.component';
+import { UserComponent } from './routes/user/user.component';
+
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  },
+  {
+    path: 'user',
+    component: UserComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: E404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
