52d46f7f791a19e8bd0e77b7732e7960f0a873af
Sanjeev Premi <spremi@ymail.com>
2018-01-07 19:24:47 +0530

    routes: user: Add sub-routes
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { AboutComponent } from './routes/about/about.component';
 import { E404Component } from './routes/e404/e404.component';
 import { HomeComponent } from './routes/home/home.component';
 import { SettingsComponent } from './routes/settings/settings.component';
+import { LoginComponent } from './routes/user/login/login.component';
+import { LogoffComponent } from './routes/user/logoff/logoff.component';
+import { RegisterComponent } from './routes/user/register/register.component';
 import { UserComponent } from './routes/user/user.component';
 
 const routes: Routes = [
@@ -22,7 +25,21 @@ const routes: Routes = [
   },
   {
     path: 'user',
-    component: UserComponent
+    component: UserComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'logoff',
+        component: LogoffComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+    ]
   },
   {
     path: '',
